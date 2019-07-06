5eabdb3b10b72ca27f8add5dd21c3003d5f7c4a9
Mike Dunston <m_dunston@comcast.net>
2019-07-06 13:22:01 -0700

    adjust uptime and task reporting metrics

--- include/stateflows/FreeRTOSTaskMonitor.h
+++ include/stateflows/FreeRTOSTaskMonitor.h
@@ -33,7 +33,7 @@ public:
    }
 private:
   StateFlowTimer timer_{this};
-  const uint64_t reportInterval_{SEC_TO_NSEC(60)};    // 1min
+  const uint64_t reportInterval_{SEC_TO_NSEC(45)};
   const uint64_t taskListInterval_{SEC_TO_USEC(300)}; // 5min
   uint64_t lastTaskList_{0};
 
@@ -45,7 +45,7 @@ private:
         "[TaskMon] uptime: %02d:%02d:%02d freeHeap: %u, largest free block: %u, tasks: %d"
       , (uint32_t)(USEC_TO_SEC(esp_timer_get_time()) / 3600)
       , (uint32_t)(USEC_TO_SEC(esp_timer_get_time()) % 3600) / 60
-      , (uint32_t)(USEC_TO_SEC(esp_timer_get_time()) - (USEC_TO_SEC(esp_timer_get_time()) % 3600))
+      , (uint32_t)(USEC_TO_SEC(esp_timer_get_time()) % 60 )
       , heap_caps_get_free_size(MALLOC_CAP_INTERNAL)
       , heap_caps_get_largest_free_block(MALLOC_CAP_8BIT)
       , taskCount
@@ -61,7 +61,7 @@ private:
       {
         LOG(INFO,
             "[TaskMon] %-16s id:%3d, prio:%2d/%2d, stack:%5d, core:%4s, "
-            "cpu%%:%6.2f, state:%s"
+            "cpu%%:%-3d, state:%s"
           , taskList[task].pcTaskName
           , taskList[task].xTaskNumber
           , taskList[task].uxCurrentPriority
@@ -70,7 +70,7 @@ private:
           , taskList[task].xCoreID == tskNO_AFFINITY ? "BOTH" :
             taskList[task].xCoreID == PRO_CPU_NUM ? "PRO" :
             taskList[task].xCoreID == APP_CPU_NUM ? "APP" : "UNK"
-          , ((float)taskList[task].ulRunTimeCounter / (float)ulTotalRunTime) * 100.0f
+          , taskList[task].ulRunTimeCounter / (ulTotalRunTime / 100)
           , taskList[task].eCurrentState == eRunning ? "Running" :
             taskList[task].eCurrentState == eReady ? "Ready" :
             taskList[task].eCurrentState == eBlocked ? "Blocked" :
