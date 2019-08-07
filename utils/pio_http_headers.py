02bfd013af07bd30d2c6e8bbbe35d547804c5657
Mike Dunston <m_dunston@comcast.net>
2019-08-07 07:20:46 -0700

    create the target directory for embedded resources before use

--- utils/pio_http_headers.py
+++ utils/pio_http_headers.py
@@ -17,30 +17,11 @@
 
 Import("env")
 
-from resource_embedder import compress, embed
+from resource_embedder import process_all
 from os.path import join
 
 def generate_embedded_resources(source, target, env):
-    compress(join(env.subst('$PROJECT_DIR'), 'data', 'index.html'),
-             join(env.subst('$PROJECT_DIR'), 'include', 'generated', 'index_html.h'),
-             'indexHtmlGz')
-    compress(join(env.subst('$PROJECT_DIR'), 'data', 'jquery.min.js'),
-             join(env.subst('$PROJECT_DIR'), 'include', 'generated', 'jquery_min_js.h'),
-             'jqueryJsGz')
-    compress(join(env.subst('$PROJECT_DIR'), 'data', 'jquery.mobile-1.5.0-rc1.min.js'),
-             join(env.subst('$PROJECT_DIR'), 'include', 'generated', 'jquery_mobile_js.h'),
-             'jqueryMobileJsGz')
-    compress(join(env.subst('$PROJECT_DIR'), 'data', 'jquery.mobile-1.5.0-rc1.min.css'),
-             join(env.subst('$PROJECT_DIR'), 'include', 'generated', 'jquery_mobile_css.h'),
-             'jqueryMobileCssGz')
-    compress(join(env.subst('$PROJECT_DIR'), 'data', 'jquery.simple.websocket.min.js'),
-             join(env.subst('$PROJECT_DIR'), 'include', 'generated', 'jquery_simple_websocket.h'),
-             'jquerySimpleWebSocketGz')
-    compress(join(env.subst('$PROJECT_DIR'), 'data', 'jqClock-lite.min.js'),
-             join(env.subst('$PROJECT_DIR'), 'include', 'generated', 'jq_clock.h'),
-             'jqClockGz')
-    embed(join(env.subst('$PROJECT_DIR'), 'data', 'ajax-loader.gif'),
-          join(env.subst('$PROJECT_DIR'), 'include', 'generated', 'ajax_loader.h'),
-          'ajaxLoader')
+  process_all(join(env.subst('$PROJECT_DIR'), 'data'),
+              join(env.subst('$PROJECT_DIR'), 'include', 'generated'))
 
 env.AddPreAction('$BUILD_DIR/src/Interfaces/WebServer.cpp.o', generate_embedded_resources)
--- utils/resource_embedder.py
+++ utils/resource_embedder.py
@@ -28,18 +28,36 @@ def serialize(sourceFile, targetFile, file, length, prefix):
                     .format(*struct.unpack("BBBBBBBBBBBBBBBB", block)))
         f.write("};\n")
 
-def compress(sourceFile, targetFile, prefix):
-    gzFile = cStringIO.StringIO()
-    with open(sourceFile) as file, gzip.GzipFile(mode='wb', fileobj=gzFile) as gz:
-        gz.writelines(file)
-    gzFile.seek(0, os.SEEK_END)
-    length = gzFile.tell()
-    gzFile.seek(0, os.SEEK_SET)
-    serialize(sourceFile, targetFile, gzFile, length, prefix)
+def compress(sourceFile, outputFileName, prefix):
+  gzFile = cStringIO.StringIO()
+  with open(sourceFile) as file, gzip.GzipFile(mode='wb', fileobj=gzFile) as gz:
+    gz.writelines(file)
+  gzFile.seek(0, os.SEEK_END)
+  length = gzFile.tell()
+  gzFile.seek(0, os.SEEK_SET)
+  serialize(sourceFile, outputFileName, gzFile, length, prefix)
 
-def embed(sourceFile, targetFile, prefix):
-    with open(sourceFile, "rb") as file:
-        file.seek(0, os.SEEK_END)
-        length = file.tell()
-        file.seek(0, os.SEEK_SET)
-        serialize(sourceFile, targetFile, file, length, prefix)
+def embed(sourceFile, outputFileName, prefix):
+  with open(sourceFile, "rb") as file:
+    file.seek(0, os.SEEK_END)
+    length = file.tell()
+    file.seek(0, os.SEEK_SET)
+    serialize(sourceFile, outputFileName, file, length, prefix)
+
+def process_all(sourceDirectory, outputDirectory):
+  if not os.path.exists(outputDirectory):
+    os.mkdir(outputDirectory)
+  compress(os.path.join(sourceDirectory, 'index.html'),
+           os.path.join(outputDirectory, 'index_html.h'), 'indexHtmlGz')
+  compress(os.path.join(sourceDirectory, 'jquery.min.js'),
+           os.path.join(outputDirectory, 'jquery_min_js.h'), 'jqueryJsGz')
+  compress(os.path.join(sourceDirectory, 'jquery.mobile-1.5.0-rc1.min.js'),
+           os.path.join(outputDirectory, 'jquery_mobile_js.h'), 'jqueryMobileJsGz')
+  compress(os.path.join(sourceDirectory, 'jquery.mobile-1.5.0-rc1.min.css'),
+           os.path.join(outputDirectory, 'jquery_mobile_css.h'), 'jqueryMobileCssGz')
+  compress(os.path.join(sourceDirectory, 'jquery.simple.websocket.min.js'),
+           os.path.join(outputDirectory, 'jquery_simple_websocket.h'), 'jquerySimpleWebSocketGz')
+  compress(os.path.join(sourceDirectory, 'jqClock-lite.min.js'),
+           os.path.join(outputDirectory, 'jq_clock.h'), 'jqClockGz')
+  embed(os.path.join(sourceDirectory, 'ajax-loader.gif'),
+        os.path.join(outputDirectory, 'ajax_loader.h'), 'ajaxLoader')
