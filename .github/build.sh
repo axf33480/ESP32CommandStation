cec52b13b0793b9cdccf41d1573b70aadcc59911
Mike Dunston <m_dunston@comcast.net>
2020-02-02 07:10:16 -0800

    :pencil: toolchain path checks

--- .github/build.sh
+++ .github/build.sh
@@ -23,11 +23,13 @@ if [ ! -f ${TOOLCHAIN_DIR}/bin/xtensa-esp32-elf-gcc ]; then
         -o ${RUN_DIR}/xtensa-esp32-elf-gcc8_2_0-esp-2019r2-linux-amd64.tar.gz
     mkdir -p ${TOOLCHAIN_DIR}
     echo "Installing toolchain..."
-    cd ${TOOLCHAIN_DIR} && tar zxf ${RUN_DIR}/xtensa-esp32-elf-gcc8_2_0-esp-2019r2-linux-amd64.tar.gz
+    cd ${TOOLCHAIN_DIR} && tar zxvf ${RUN_DIR}/xtensa-esp32-elf-gcc8_2_0-esp-2019r2-linux-amd64.tar.gz
+    echo "Adding ${TOOLCHAIN_DIR}/bin to the path"
+    # add toolchain to the path
+    export PATH=${TOOLCHAIN_DIR}/bin:${PATH}
+    ls -l ${TOOLCHAIN_DIR}/bin
 fi
 
-# add toolchain to the path
-export PATH=${TOOLCHAIN_DIR}/bin:${PATH}
 
 # clone ESP-IDF
 if [ ! -d ${IDF_PATH} ]; then
